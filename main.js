ce93dee7a773c92aa054a931e57ba9f26c2796e7
Aditya Agarwal <adityaagarwal382@gmail.com>
2026-01-14 07:11:01 +0530

    Document JavaScript basics in main.js
    Added JavaScript fundamentals and examples.

---
+++ main.js
@@ -0,0 +1,28 @@
+Javascript controls logic and behaviour of websites.
+Js is a single threaded interpreted dynamic language which runs line by line and moves fast
+Variables - containers used to store data values - let, var, const
+Let value can be changed, const value remains constant.
+Data types - define the nature of data stored. Primitive and non-primtive.
+Primitive data types - include String Number Boolean Null Undefined
+Non Primitve Complex data types - include Arrays Obljects Functions
+Arrays - single variable used to store an ordered collection.
+Objects - used to store collections of related data & functionaility/ key-value pair.
+Operators - include + - * / %= == === > < >= <= && || !
+If/else statemnts - conditional statements which execute if specified condition is true or false.
+Loops - structures that repeat a block of code multiple times until a specified condition is met.
+Functions - Set of stmts that performs a task or calculates a value.Takes some input and return an output.
+Callback function - A function passed as an argument to another function.
+Anonymous function - A function that does not have a name associated with it./ Defined inline, used for one-time tasks.
+Async function - Multiple things are running parallely at the same time, context switching with each other, runs in parts.
+Sync function - Sequential, only one thing is happening at a time.
+Javascript can context witch using async functions.
+
+Examples - 
+Variables - let, const,
+ let a = 1; console.log(a);
+Loops - for(let i=0; i<5; i++){
+	console.log(i);
+Array - let fruits = [apple, orange, banana]
+Objects - Key:Value pair
+	let person = { name:aditya, age:21, gender:male }
+Functions Calculate Sum(n)=
