37fe34cd1c0fa38960817c4bcf7252016c7070f5
Aditya Agarwal <adityaagarwal382@gmail.com>
2026-01-15 08:51:37 +0530

    Add JavaScript concepts and examples to main.js
    Add JavaScript concepts and examples to main.js

--- main.js
+++ main.js
@@ -16,13 +16,55 @@ Anonymous function - A function that does not have a name associated with it./ D
 Async function - Multiple things are running parallely at the same time, context switching with each other, runs in parts.
 Sync function - Sequential, only one thing is happening at a time.
 Javascript can context witch using async functions.
-
+Async - makes a function return a Promise
+Await - pauses execution until a Promise resolves
+Promise - represents a value that will be available in the future
+Map - creates a new array by transforming each item
+Filter - creates a new array with only matching items
 Examples - 
-Variables - let, const,
- let a = 1; console.log(a);
-Loops - for(let i=0; i<5; i++){
+Variables - 
+	let, const,
+    let a = 1; console.log(a);
+Loops - 
+	for(let i=0; i<5; i++){
 	console.log(i);
-Array - let fruits = [apple, orange, banana]
-Objects - Key:Value pair
+Array - 
+	let fruits = [apple, orange, banana]
+Objects - 
+	Key:Value pair
 	let person = { name:aditya, age:21, gender:male }
-Functions Calculate Sum(n)=
+Function - 
+	function calculatesum (a,b) {
+     return a + b;
+    }
+    let a = calculatesum(2,3)
+    console.log(a)
+Callback Function - 
+	function calculate(a, b, callback) {
+    let result = a + b;
+    callback(result);
+    }
+    function display(result) {
+    console.log("Result is:", result);
+    }
+    calculate(5, 3, display);
+Async + Await -
+	async function greet() {
+    let message = await Promise.resolve("Hello World");
+    console.log(message);
+    } 
+	greet();
+Promise -
+	const promise = new Promise((resolve, reject) => {
+    resolve("Success!");
+    });
+    promise.then(result => {
+    console.log(result);
+    });
+Map -
+	const numbers = [1, 2, 3, 4, 5];
+	const doubled = numbers.map(num => num * 2);
+    console.log(doubled);
+Filter -
+	const evenNumbers = numbers.filter(num => num % 2 === 0);
+    console.log(evenNumbers);
