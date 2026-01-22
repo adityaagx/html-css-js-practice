af0b01848ec6761a01a346ac90400534e9039df6
Aditya Agarwal <adityaagarwal382@gmail.com>
2026-01-22 07:06:50 +0530

    Fix comments for querySelector usage examples
    Fix comments for querySelector usage examples

--- dom-cheatsheet.js
+++ dom-cheatsheet.js
@@ -24,9 +24,9 @@ document.getElementsByClassName("class");
 document.getElementsByTagName("p");
 
 // Modern selectors (recommended)
-document.querySelector("#id");     // tag
+document.querySelector("#id");     // id
 document.querySelector(".class");  // class
-document.querySelector("p");       // id
+document.querySelector("p");       // tag
 
 document.querySelectorAll(".class"); // multiple
 
